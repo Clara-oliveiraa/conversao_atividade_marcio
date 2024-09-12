8b88dc2b81abb6f813440011dee21af96a8b84b3
Clara Oliveira <claraoliveira.8351@gmail.com>
2024-09-12 19:54:46 -0300

    Add files via upload

---
+++ conversao-temperatura/src/test/java/com/example/conversao_temperatura/ConversaoTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conversao_temperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversaoTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
